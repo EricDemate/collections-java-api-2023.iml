05e83724c5d8c99ff3286fd8075711082bacce51
Eric L Demate <eric.demate@gmail.com>
2024-06-25 21:33:20 -0300

    Resolvendo desafio ListaTarefa
    Signed-off-by: Eric L Demate <eric.demate@gmail.com>

---
+++ src/list/OperacoesBasicas/ListaTarefa.java
@@ -0,0 +1,50 @@
+package list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    //Atributo Lista Tarefa
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao) {
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for (Tarefa t : tarefaList) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefasParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+
+    public int obterNumeroTotalTarefas() {
+        return tarefaList.size();
+
+    }
+
+    public void obterdescricoestarefas() {
+        System.out.println(tarefaList);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+        System.out.println("O numero total de alementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+        listaTarefa.adicionarTarefa("tarefa 1");
+        System.out.println("O numero total de alementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.adicionarTarefa("tarefa 1");
+        listaTarefa.adicionarTarefa("tarefa 2");
+        System.out.println("O numero total de alementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+        listaTarefa.removerTarefa("tarefa 1");
+        System.out.println("O numero total de alementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+    }
+}
